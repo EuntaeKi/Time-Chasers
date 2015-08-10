fe47e41e1aad2deb3b6b6347f0bc77d72f378df2
Alan <alanaphayarath@gmail.com>
2015-08-09 22:55:09 -0700

    idk

--- src/GameBackground.java
+++ src/GameBackground.java
@@ -14,6 +14,9 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+
+
+
 /** @see http://stackoverflow.com/questions/4148336 */
 public class GameBackground extends JPanel {
     GameBackground(Digit digit) {
@@ -22,24 +25,49 @@ public class GameBackground extends JPanel {
         add(cellValue, BorderLayout.CENTER);
     }
 
+    
+    
+/*    
+    public static void main(String[] args) {
+        ImagePanel panel = new ImagePanel(new ImageIcon("img/Clock Equal.png").getImage());
+        
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(panel);
+        frame.setSize(1080, 1080);
+        //frame.pack();
+        frame.setVisible(true);
+      }
+    }
+     
+    class ImagePanel extends JPanel {
+     
+      private Image img;
+     
+      public ImagePanel(String img) {
+        this(new ImageIcon(img).getImage());
+      }
+     
+      public ImagePanel(Image img) {
+        this.img = img;
+        Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+        setPreferredSize(size);
+        setMinimumSize(size);
+        setMaximumSize(size);
+        setSize(size);
+        setLayout(null);
+      }
+     
+      public void paintComponent(Graphics g) {
+        g.drawImage(img, 0, 0, null);
+      }
+    
+    
+    
+    
+    */
+    
+    
 class JDigit extends JPanel {
-	
-	private Image img;
-	 
-	  public ImagePanel(String img) {
-	    this(new ImageIcon(img).getImage());
-	  }
-	 
-	  public ImagePanel(Image img) {
-	    this.img = img;
-	    Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
-	    setPreferredSize(size);
-	    setMinimumSize(size);
-	    setMaximumSize(size);
-	    setSize(size);
-	    setLayout(null);
-	  }
-	 
 
     public JDigit(Digit digit) {
         this.setPreferredSize(new Dimension(276, 500));
@@ -48,7 +76,7 @@ class JDigit extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         g.drawOval(0, 0, 50, 50);
-        g.drawImage(img, 0, 0, null);
+       // g.drawImage(img, 0, 0, null);
     }
   }
 }
